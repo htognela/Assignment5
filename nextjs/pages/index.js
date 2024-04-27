9afebdcc86d8ac3866a2db7a36a4ce216a3b61cf
htognela <157424920+htognela@users.noreply.github.com>
2024-04-27 11:21:20 -0500

    Update index.js

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -2,9 +2,6 @@ import Layout from "../components/layout";
 import Image from "next/image";
 import Link from 'next/link';
 
-import Layout from "../components/layout";
-import Link from 'next/link';
-
 export default function Home() {
     return (
         <Layout>
