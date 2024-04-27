b6d185b26e3b53f004a19ae70014bcc844f81175
htognela <157424920+htognela@users.noreply.github.com>
2024-04-27 11:58:27 -0500

    Update index.js

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
         <Layout>
             <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-r from-cyan-500 to-blue-500">
                 <h1 className="text-4xl font-bold text-white mb-4">Welcome to Our DVD Rental Store</h1>
-                <Link href='/AddCustomer'>
-                    <a className="text-lg text-indigo-200 hover:text-indigo-100">Add New Customer</a>
+                <Link href='/AddCustomer' className="text-lg text-indigo-200 hover:text-indigo-100">
+                    Add New Customer
                 </Link>
             </main>
         </Layout>
